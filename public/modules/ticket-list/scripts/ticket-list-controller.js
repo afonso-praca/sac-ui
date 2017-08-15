60a9ea93acf8f12c83f543ba706dcee48555aa4c
afonso <afonsoinfo@gmail.com>
2017-08-14 23:58:49 -0300

    uses ticket list service to get data from API

--- public/modules/ticket-list/scripts/ticket-list-controller.js
+++ public/modules/ticket-list/scripts/ticket-list-controller.js
@@ -1,10 +1,15 @@
 angular.module('ticketList', [])
-  .controller("TicketListController", function($scope) {
+  .controller("TicketListController", function($scope, TicketListService) {
 
     var self = this;
 
     self.startModule = function() {
-      console.log("Ticket List Started!")
+      console.log("Ticket List Started!");
+      TicketListService.getTickets().then(function (data) {
+        console.log(data);
+      }, function (error) {
+        throw new Error(error);
+      });
     };
 
     $scope.editTicket = function(tpRule, rule) {
