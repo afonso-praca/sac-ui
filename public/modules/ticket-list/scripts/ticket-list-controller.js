6ff7bb04963e4adc8b7b87009deb393fa92e4c21
afonso <afonsoinfo@gmail.com>
2017-08-15 00:48:40 -0300

    put ticket list in scope and use it in template

--- public/modules/ticket-list/scripts/ticket-list-controller.js
+++ public/modules/ticket-list/scripts/ticket-list-controller.js
@@ -2,11 +2,13 @@ angular.module('ticketList', [])
   .controller("TicketListController", function($scope, TicketListService) {
 
     var self = this;
+    $scope.tickets = [];
 
     self.startModule = function() {
       console.log("Ticket List Started!");
       TicketListService.getTickets().then(function (data) {
         console.log(data);
+        $scope.tickets = data;
       }, function (error) {
         throw new Error(error);
       });
