6809d915453d8ad22c988562842a8e7dffbd0a25
afonso <afonsoinfo@gmail.com>
2017-08-14 23:18:17 -0300

    create angular app route system

---
+++ public/app.js
@@ -0,0 +1,42 @@
+// APP MAIN
+angular.module("app", ["ngRoute", "ticketList"])
+
+  .config(function($routeProvider, $compileProvider, $httpProvider) {
+
+    $compileProvider.debugInfoEnabled(false);
+
+    // TICKET LIST
+    $routeProvider.when('/tickets/', {
+      controller: "TicketListController",
+      templateUrl: 'modules/ticket-list/views/ticket-list-view.html',
+      // resolve: {
+      //   tickets: function(TicketListService) {
+      //     return TicketListService.getTickets();
+      //   }
+      // }
+    });
+
+    $routeProvider.otherwise({
+      redirectTo: '/tickets/'
+    });
+
+    // HTTP SETTINGS
+    $httpProvider.useApplyAsync(true);
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+  })
+
+  .run(function($rootScope) {
+
+    $rootScope.$on('$routeChangeSuccess', function(){
+      // stop loader
+    });
+    $rootScope.$on('$routeChangeStart', function(){
+      // start loader
+    });
+
+  });
+
+// BOOTSTRAPS ANGULAR APP
+angular.element(document).ready(function(){
+  angular.bootstrap(document.getElementById("app-container"), ['app']);
+});
