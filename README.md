a87f0fe916c045780cbcd2c1e9074a97b15d8900
afonso <afonsoinfo@gmail.com>
2017-08-16 01:09:01 -0300

    improves README

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # sac-ui
+
+Front end app
+Depends on a running instance of sac-api to properly run.
+
+## How to run
+
+- `npm i`
+- `npm start`
+
+then, access it in `http://localhost:3000/`
--- server.js
+++ server.js
@@ -4,10 +4,6 @@ var path = require('path');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.get('/', function (req, res) {
-//   res.send('Hello World!');
-// });
-
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('SAC App UI Server listening on port 3000!');
 });
